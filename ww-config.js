d205833deb041759b00db80cf2bfa6984010849d
Léo Coletta <mail.leo.coletta@gmail.com>
2025-01-14 22:30:47 +0100

    feat: add copilot configuration

--- ww-config.js
+++ ww-config.js
@@ -1,5 +1,20 @@
 export default {
     editor: {
         designSystemId: 'eb160add-c723-4258-9686-e8c31e04cd86',
+        /* wwEditor:start */
+        settings: {
+            copilot: {
+                description: 'Configure the Typeform plugin settings',
+                returns: 'void',
+                schema: {
+                    designSystemId: {
+                        type: 'string',
+                        description: 'The unique identifier for the design system',
+                        bindable: false
+                    }
+                }
+            }
+        }
+        /* wwEditor:end */
     },
 };
